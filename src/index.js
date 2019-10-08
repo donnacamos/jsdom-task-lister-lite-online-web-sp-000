6f69aab330faecc85b2870979d601b5a250ff7b9
Donna Amos <donnacamos.developer@gmail.com>
2019-10-08 15:06:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("new-task-description").innerHTML += "Sorry! <code>preventDefault()</code> won't let you submit this!<br>";
   event.preventDefault();
-}, false); 
+}, false);
